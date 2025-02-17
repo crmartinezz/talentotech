1ee084088e2a5f2c6568245d40c8c7e13ed78a73
crmartinezz <criscamilox@gmail.com>
2025-02-17 07:12:24 -0500

    talentotech_app.py

--- talentotech_app.py
+++ talentotech_app.py
@@ -1,5 +1,15 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib as mtl
+import seaborn as sb
+import plotly as sns
+import datetime as dt
 #configuración inicial de la aplicación
 st.set_page_config(
   page_title="Dashboard Interactivo",
-  page_icon=""
+  page_icon="📊"
+  layout="wide"
+)
+st.title("Dashboard nteractivo con Streamlit")
+st.sidebar.title("🔍 Opciones de navegación")
